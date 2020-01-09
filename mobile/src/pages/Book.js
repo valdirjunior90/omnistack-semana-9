328f61d2201a7e400704d3f40d3493ba273ee783
Junitrollnita <Junitrollnita@users.noreply.github.com>
2020-01-09 06:49:35 -0300

    App terminado

---
+++ mobile/src/pages/Book.js
@@ -0,0 +1,96 @@
+import React, {useState} from 'react'
+import {SafeAreaView,Alert,StyleSheet,TextInput, Text, TouchableOpacity, AsyncStorage} from 'react-native'
+
+import api from '../services/api'
+
+export default function Book({navigation}) {
+    const [date,setDate] = useState('');
+    const id = navigation.getParam('id');
+
+   async function handleSubmit(){
+        const user_id = await AsyncStorage.getItem('user');
+        
+        await api.post(`/spots/${id}/bookings`,{
+            date
+        }, {
+            hearders: { user_id }
+        })
+        
+        Alert.alert('Solicitaão de Reserva enviada!');
+
+        navigation.navigate('List')
+
+    }
+
+    function handleCancel(){
+        navigation.navigate('List')
+    }
+
+return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.label}>DATA DE INTERESSE *</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Qual data você quer reservar?"
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false}
+        value={date}
+        onChangeText={setDate}
+      />
+
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Solicitar reserva</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleCancel} style={[styles.button, styles.cancelButton]}>
+        <Text style={styles.buttonText}>Cancelar</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+
+)
+}
+
+const styles = StyleSheet.create({
+    container:{
+        margin:30,
+    },
+    
+    label:{
+        fontWeight:'bold',
+        color:'#444',
+        marginBottom:8,
+        marginTop:20,
+    },
+
+    input:{
+        borderWidth:1,
+        borderColor:'#ddd',
+        paddingHorizontal: 20,
+        fontSize:15,
+        color:'#444',
+        height:44,
+        marginBottom: 20,
+        borderRadius:2,
+
+    },
+
+    button:{
+        height:42,
+        backgroundColor:'#f05a5b',
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:2,
+    },
+    cancelButton:{
+        backgroundColor:'#ccc',
+        marginTop:10,
+    },
+
+    buttonText:{
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16,
+
+    }
+})
---
+++ mobile/src/routes.js
@@ -0,0 +1,17 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+//importação de paginas
+import Login from './pages/Login';
+import List from './pages/List';
+import Book from './pages/Book';
+
+
+const Routes = createAppContainer(
+    createSwitchNavigator({
+        Login,
+        List,
+        Book
+    })
+);
+
+
+export default Routes;
